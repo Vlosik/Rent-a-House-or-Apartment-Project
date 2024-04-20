1f416dd59a7732cd237b403c8cfc7c34e8367430
Adrian Vlose <113776224+Vlosik@users.noreply.github.com>
2024-04-20 16:37:04 +0300

    Update ServicePropertyTests.java

--- src/test/java/Proiect_PS/ServicePropertyTests.java
+++ src/test/java/Proiect_PS/ServicePropertyTests.java
@@ -1,4 +1,23 @@
 package Proiect_PS;
 
+import Proiect_PS.repository.RepositoryProperty;
+import Proiect_PS.repository.RepositoryUser;
+import Proiect_PS.service.ServiceProperty;
+import org.junit.Before;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Mockito.verify;
+
 public class ServicePropertyTests {
+    private ServiceProperty property;
+    @Mock
+    private RepositoryProperty repositoryProperty;
+    @Mock
+    private RepositoryUser repositoryUser;
+    @Before
+    public void setUp(){
+        MockitoAnnotations.initMocks(this);
+        property = new ServiceProperty(repositoryProperty,repositoryUser);
+    }
 }
